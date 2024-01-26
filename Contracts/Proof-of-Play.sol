31d64b18c1859a27bcb8a657ea48e1cd1da0dd1a
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2024-01-27 00:37:02 +0100

    Update Proof-of-Play.sol

--- Contracts/Proof-of-Play.sol
+++ Contracts/Proof-of-Play.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
+
 interface IBattledog {
     struct Player {
         string name;
@@ -15,8 +21,9 @@ interface IBattledog {
         uint256 history;
     }
 
-    function getPlayers() external view returns (Player[] memory);
+    function balanceOf(address _caller) external view returns (uint256);
     function getPlayerOwners(address _user) external returns (Player[] memory);
+    function ownerOf(uint256 _index) external view returns (address);
     function blacklisted(uint256 _index) external view returns (bool);
 }
 
@@ -37,6 +44,7 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
     uint256 public winsbonus = 3;
     uint256 public fightsbonus = 2;
     uint256 public historybonus = 1;
+    uint256 private startTime;
         
 
     // Declare the ActiveMiners & Blacklist arrays
@@ -45,7 +53,7 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
     mapping(uint256 => IBattledog.Player) public ActiveMiners;
     mapping(uint256 => Miner) public Collectors;
     mapping(uint256 => uint256) public MinerClaims;
-
+    mapping(uint256 => bool) private minerstate;
 
     struct Miner {
         string name;
@@ -70,6 +78,7 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
         GAMEToken = IERC20(_GAMEToken);
         battledogs = _battledogs;
         guard = _newGuard;
+        startTime = block.timestamp;
     }
 
     using ABDKMath64x64 for uint256;  
@@ -80,12 +89,22 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
     }
 
     function getMinerData() public nonReentrant {
-        IBattledog.Player[] memory players = IBattledog(battledogs).getPlayers();
-        activeMinersLength = players.length;
+    uint256 total = IBattledog(battledogs).balanceOf(msg.sender);
+    IBattledog.Player[] memory players = IBattledog(battledogs).getPlayerOwners(msg.sender);
+    
+      for (uint256 i = 0; i < total; i++) {
+        uint256 tokenId = players[i].id;
 
-        for (uint256 i = 0; i < players.length; i++) {
-            ActiveMiners[i] = players[i];
+        // Create a new instance of the Miner struct with the player's data
+        ActiveMiners[tokenId] = players[i];
+        
+        // Increment the activeMinersLength
+        if (!minerstate[tokenId]) {
+          activeMinersLength++;
         }
+
+        minerstate[tokenId] = true;
+      }
     }
 
     function getActiveMiner(uint256 index) public view returns (IBattledog.Player memory) {
@@ -93,50 +112,75 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
         return ActiveMiners[index];
     }
 
-    function getOwnerData(uint256 _tokenId) internal returns (bool) {        
-    // Get the Player structs owned by the msg.sender
-    IBattledog.Player[] memory owners = IBattledog(battledogs).getPlayerOwners(msg.sender);
-
-    // Iterate through the stored Player structs and compare the _tokenId with the id of each Player struct
-        for (uint256 i = 0; i < owners.length; i++) {
-            if (owners[i].id == _tokenId) {
+    function getOwnerData(uint256 _tokenId) internal view returns (bool) {        
+    // Get the token id owned by the msg.sender
+      address owner = IBattledog(battledogs).ownerOf(_tokenId);
+        // compare the _tokenId 
+        if (owner == msg.sender) {
                 return true;
-            }
-        }
-        return false;
+              }        
+          return false;
     }
 
     function mineGAME(uint256[] calldata _nfts) public nonReentrant {
-        // Require Contract isn't paused
-        require(!paused, "Paused Contract");
+    // Require Contract isn't paused
+    require(!paused, "Paused Contract");
+    // Populate the ActiveMiners array
+    uint256 total = IBattledog(battledogs).balanceOf(msg.sender);
+    IBattledog.Player[] memory players = IBattledog(battledogs).getPlayerOwners(msg.sender);
+    
+    for (uint256 i = 0; i < total; i++) {
+        uint256 tokenId = players[i].id;
+        // Create a new instance of the Miner struct with the player's data
+        ActiveMiners[tokenId] = players[i];
+        
+        // Increment the activeMinersLength
+        if (!minerstate[tokenId]) {
+          activeMinersLength++;
+          minerstate[tokenId] = true;
+        }
+
+    }
 
         for (uint256 a = 0; a < _nfts.length; a++) {
             uint256 tokenId = _nfts[a]; // // Current NFT id
             // Require Token Ownership    
             require(getOwnerData(tokenId), "Not Owner");        
-            // Require Miner hasn't claimed within timelock
-            require(MinerClaims[tokenId] + timeLock < block.timestamp, "Timelocked");
+            // Require Miner hasn't claimed within timelock  
+              uint256 unlock = MinerClaims[tokenId] + timeLock;  
+              uint256 currentTime = block.timestamp;
+            require(unlock < currentTime, "Timelocked");
             // Require Miner is not on blacklist
             require(!IBattledog(battledogs).blacklisted(tokenId), "NFT Blacklisted");
-            // Reorganize ActiveMiners array
-            IBattledog.Player[] memory players = IBattledog(battledogs).getPlayers();
-            activeMinersLength = players.length;
-
-            for (uint256 i = 0; i < players.length; i++) {
-                      ActiveMiners[i] = players[i];
-                }
 
             // Check if the miner is activated
             if (ActiveMiners[tokenId].activate > 0) {
+            uint256 freq; uint256 activatefactor; uint256 activate; uint256 level; 
+            uint256 fights; uint256 wins; uint256 history; uint256 rewards;
 
-                // Calculate Rewards
-                uint256 activatefactor = (ActiveMiners[tokenId].activate - 1) * activatebonus;
-                uint256 activate = ((ActiveMiners[tokenId].activate - 1)) * multiplier;
-                uint256 level = ((ActiveMiners[tokenId].level - Collectors[tokenId].level) * levelbonus) + activatefactor;
-                uint256 fights = ((ActiveMiners[tokenId].fights - Collectors[tokenId].fights) * fightsbonus) + activatefactor;
-                uint256 wins = ((ActiveMiners[tokenId].wins - Collectors[tokenId].wins) * winsbonus) + activatefactor;
-                uint256 history = ((ActiveMiners[tokenId].history - Collectors[tokenId].history) * historybonus) + activatefactor;
-                uint256 rewards = (activate + level + fights + wins + history) * divisor;
+              // Calculate Rewards
+              if (MinerClaims[tokenId] > 0) {                
+              freq = (currentTime - MinerClaims[tokenId]) / timeLock;
+              } else if (((currentTime - startTime) / timeLock) < 0) {
+                freq = 1;
+              } else {
+                freq = ((currentTime - startTime) / timeLock);
+              }
+
+              activatefactor = (ActiveMiners[tokenId].activate - 1) * activatebonus;
+              activate = ((ActiveMiners[tokenId].activate - 1)) * multiplier * freq;
+              level = ((ActiveMiners[tokenId].level - Collectors[tokenId].level) * levelbonus) + activatefactor;
+              
+              if (ActiveMiners[tokenId].fights > Collectors[tokenId].fights) {  
+              fights = ((ActiveMiners[tokenId].fights - Collectors[tokenId].fights) * fightsbonus) + activatefactor;
+              }
+
+              if (ActiveMiners[tokenId].wins > Collectors[tokenId].wins) {
+              wins = ((ActiveMiners[tokenId].wins - Collectors[tokenId].wins) * winsbonus) + activatefactor;
+              }
+
+              history = ((ActiveMiners[tokenId].history - Collectors[tokenId].history) * historybonus) + activatefactor;
+              rewards = (activate + level + fights + wins + history) * divisor;
 
                 // Check the contract for adequate withdrawal balance
                 require(GAMEToken.balanceOf(address(this)) > rewards, "Not Enough Reserves");      
@@ -145,7 +189,7 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
                 // Register claim
                 getCollectors(tokenId);
                 // Register claim timestamp
-                MinerClaims[tokenId] = block.timestamp; // Record the miner's claim timestamp
+                MinerClaims[tokenId] = currentTime; // Record the miner's claim timestamp
                 // TotalClaimedRewards
                 totalClaimedRewards += rewards;       
                 // Emit event
@@ -174,6 +218,25 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
             activeMiner.history
         );
     }
+    
+    function setCollectors (string memory _name, uint256 _id, uint256 _level, 
+    uint256 _attack, uint256 _defence, uint256 _fights, uint256 _wins, 
+    uint256 _payout, uint256 _activate, uint256 _history, uint256 _time) external onlyOwner nonReentrant {
+       Collectors[_id] = Miner(
+        _name,
+        _id,
+        _level,
+        _attack,
+        _defence,
+        _fights,
+        _wins,
+        _payout,
+        _activate,
+        _history
+        );  
+        
+      MinerClaims[_id] = _time; // Record the miner's claim timestamp
+    }
 
     function setTimeLock(uint256 _seconds) external onlyOwner {
         timeLock = _seconds;
@@ -181,6 +244,10 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
 
     function setMultiplier (uint256 _multiples) external onlyOwner() {
         multiplier = _multiples;
+    }
+
+    function setTime(uint256 _time) external onlyOwner {
+      startTime = _time;
     }
 
     function setBonuses (uint256 _activate, uint256 _level, uint256 _wins, uint256 _fights, uint256 _history) external onlyOwner() {
@@ -221,4 +288,4 @@ contract ProofOfPlay is Ownable, ReentrancyGuard {
         paused = false;
         emit Unpause();
     } 
-}              
+}                      
